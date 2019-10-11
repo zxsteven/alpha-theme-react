94cd5f0ca24d84bf10d76b6819393d172b3753a8
zxsteven <breathesteve@gmail.com>
2019-10-10 19:54:14 -0500

    added react.lazy, Suspense, and loading spinner

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
-import { Route } from 'react-router-dom'
-
-import Header from './components/header/header.component'
-import HomePage from './pages/HomePage/homepage.component'
-import ContactPage from './pages/ContactPage/contactpage.component'
-import GenericPage from './pages/Generic/genericpage.component'
-import ElementsPage from './pages/Elements/elementspage.component'
+import React, { lazy, Suspense } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
+
+import Header from './components/header/header.component'
+import Spinner from './components/spinner/spinner.component'
+
+const HomePage = lazy(() => import('./pages/HomePage/homepage.component'))
+const GenericPage = lazy(() => import('./pages/Generic/genericpage.component'))
+const ElementsPage = lazy(() => import('./pages/Elements/elementspage.component'))
+const ContactPage = lazy(() => import('./pages/ContactPage/contactpage.component'))
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/generic' component={GenericPage} />
-      <Route exact path='/elements' component={ElementsPage} />
-      <Route exact path='/contact' component={ContactPage} />
+      <Switch>
+        <Suspense fallback={<Spinner />}>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/generic' component={GenericPage} />
+          <Route exact path='/elements' component={ElementsPage} />
+          <Route exact path='/contact' component={ContactPage} />
+        </Suspense>
+      </Switch>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import './header.styles.scss'
-
 
 const Header = () => {
   return (
